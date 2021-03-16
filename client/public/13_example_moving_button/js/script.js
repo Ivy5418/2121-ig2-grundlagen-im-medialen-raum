40063d6ddcc5786aed926c2097236937656e0576
Vincent-Aaron-Paul <75975370+Vincent-Aaron-Paul@users.noreply.github.com>
2021-03-16 14:24:29 +0100

    r
    f

--- client/public/13_example_moving_button/js/script.js
+++ client/public/13_example_moving_button/js/script.js
@@ -1,46 +1,42 @@
-// Connecting to server. Don't touch this :-) 
+// Connecting to server. Don't touch this :-)
 let socket = io();
 
-
-
 function handleButtonClick() {
-    // console.log("button wurde geklickt");
-    socket.emit('serverEvent', "süd");
+  // console.log("button wurde geklickt");
+  socket.emit("serverEvent", "nord");
 }
 
-
-socket.on('connected', function (msg) {
-    console.log(msg);
+socket.on("connected", function (msg) {
+  console.log(msg);
 });
 
-// Incoming events 
-socket.on('serverEvent', function (message) {
-    console.log(message);
+// Incoming events
+socket.on("serverEvent", function (message) {
+  console.log(message);
 
-    let button1 = document.getElementById("button1");
+  let button1 = document.getElementById("button1");
 
-    if (message == "süd") {
-        let y = button1.offsetTop;
-        y = y + 20;
-        button1.style.top = y + "px";
-    }
+  if (message == "süd") {
+    let y = button1.offsetTop;
+    y = y + 20;
+    button1.style.top = y + "px";
+  }
 
-    if (message == "nord") {
-        let y = button1.offsetTop;
-        y = y - 20;
-        button1.style.top = y + "px";
-    }
+  if (message == "nord") {
+    let y = button1.offsetTop;
+    y = y - 20;
+    button1.style.top = y + "px";
+  }
 
-    if (message == "ost") {
-        let x = button1.offsetLeft;
-        x = x - 20;
-        button1.style.left = x + "px";
-    }
+  if (message == "ost") {
+    let x = button1.offsetLeft;
+    x = x - 20;
+    button1.style.left = x + "px";
+  }
 
-    if (message == "west") {
-        let x = button1.offsetLeft;
-        x = x + 20;
-        button1.style.left = x + "px";
-    }
-
+  if (message == "west") {
+    let x = button1.offsetLeft;
+    x = x + 20;
+    button1.style.left = x + "px";
+  }
 });
