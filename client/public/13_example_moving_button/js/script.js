8ff04dcdfcd32a4d98075122c6f006c294c42268
Vincent-Aaron-Paul <75975370+Vincent-Aaron-Paul@users.noreply.github.com>
2021-03-16 15:44:23 +0100

    Update script.js

--- client/public/13_example_moving_button/js/script.js
+++ client/public/13_example_moving_button/js/script.js
@@ -1,24 +1,19 @@
 // Connecting to server. Don't touch this :-)
 let socket = io();
-<<<<<<< HEAD
-=======
-let readysüd = false
-let readynord = false
-let readyost = false
-let readywest = false
 
->>>>>>> afd807aeb00979d877c1446626abc66721e62e84
+let readysüd = false;
+let readynord = false;
+let readyost = false;
+let readywest = false;
 
 function handleButtonClick() {
   // console.log("button wurde geklickt");
-  socket.emit("serverEvent", "nord");
+  socket.emit("serverEvent", "ost");
 }
 
 socket.on("connected", function (msg) {
   console.log(msg);
 });
-
-<<<<<<< HEAD
 // Incoming events
 socket.on("serverEvent", function (message) {
   console.log(message);
@@ -27,59 +22,25 @@ socket.on("serverEvent", function (message) {
 
   if (message == "süd") {
     let y = button1.offsetTop;
-    y = y + 20;
-    button1.style.top = y + "px";
+    let readysüd = true;
   }
 
   if (message == "nord") {
     let y = button1.offsetTop;
-    y = y - 20;
-    button1.style.top = y + "px";
+    let readynord = true;
   }
 
   if (message == "ost") {
     let x = button1.offsetLeft;
-    x = x - 20;
-    button1.style.left = x + "px";
+    let readyost = true;
   }
 
   if (message == "west") {
     let x = button1.offsetLeft;
-    x = x + 20;
-    button1.style.left = x + "px";
+    let readywest = true;
   }
-=======
-// Incoming events 
-socket.on('serverEvent', function (message) {
-    console.log(message);
 
-    let button1 = document.getElementById("button1");
-
-    if (message == "süd") {
-        let y = button1.offsetTop;
-        let readysüd = true;
-    }
-
-    if (message == "nord") {
-        let y = button1.offsetTop;
-        let readynord = true;
-    }
-
-    if (message == "ost") {
-        let x = button1.offsetLeft;
-        let readyost = true;
-    }
-
-    if (message == "west") {
-        let x = button1.offsetLeft;
-        let readywest = true;
-    }
-
-    if (readysüd === true && readynord === true && readyost === true && readywest === true ) {
-        console.log("funktioniert");
-    
- 
-    }
-
->>>>>>> afd807aeb00979d877c1446626abc66721e62e84
+  if (readysüd === true && readynord === true && readyost === true && readywest === true) {
+    console.log("funktioniert");
+  }
 });
