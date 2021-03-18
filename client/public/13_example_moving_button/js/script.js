f71433af3240ffd222218e305a5de3f499839cae
lennart.paul <lennart.paul@bevelop.de>
2021-03-18 18:22:04 +0100

    @reset function

--- client/public/13_example_moving_button/js/script.js
+++ client/public/13_example_moving_button/js/script.js
@@ -1,13 +1,10 @@
 // Connecting to server. Don't touch this :-)
 let socket = io();
 
+let gameState = "STOP";
+
 let myID, myIndex;
 let userList = [];
-
-let readysüd = false;
-let readynord = false;
-let readyost = false;
-let readywest = false;
 
 let colorRow = [
   "#534E8C",
@@ -25,66 +22,142 @@ let intervalID;
 let counter = 0;
 let clickCounter = 0;
 
+function getMyColor() {
+  switch (myIndex) {
+    case 0:
+      return "#534E8C";
+    case 1:
+      return "#E4AD27";
+    case 2:
+      return "#FB8D8F";
+    case 3:
+      return "#398B9D";
+    default:
+      console.error("Invalid player index");
+      break;
+  }
+}
+
+function clickOnColor(color) {
+  const myColor = getMyColor();
+
+  if (myColor === color) {
+    console.log("RIGHT");
+
+    // Emit the correct button press
+    socket.emit("serverEvent", { type: "rightColor" });
+
+    // Hide Button after correct press
+    $("#colorButton").hide();
+  } else if (myColor != color) {
+    console.log("WRONG COLOR");
+
+    // Emit the wrong button press
+    socket.emit("serverEvent", { type: "wrongColor" });
+
+    // Hide Button after wrong press
+    $("#colorButton").hide();
+  }
+}
+
+/**
+ * Change the color of the given button
+ * @param {*} changeColorRow
+ */
 function changeColor(changeColorRow) {
-  console.log(counter);
+  if (counter === 6) {
+    resetGame();
+  }
 
-  $(".button").css("display", "none");
-  $(".buttonviolet").css("display", "none");
-  $(".buttonyellow").css("display", "none");
-  $(".buttonblue").css("display", "none");
-  $(".buttonpink").css("display", "none");
-  $("body").css("background-color", changeColorRow[counter]);
+  if (gameState === "RUNNING" && counter < 7) {
+    counter++;
+    $(".buttonviolet").hide();
+    $(".buttonyellow").hide();
+    $(".buttonblue").hide();
+    $(".buttonpink").hide();
+    $("body").css("background-color", changeColorRow[counter]);
 
-  counter++;
+    $("#colorButton").show();
+    $("#colorButton").prop("value", changeColorRow[counter]);
 
-  if (counter < 7) {
     setTimeout(() => {
       changeColor(changeColorRow);
     }, 1000);
   }
 }
 
-// intervalID = window.setInterval(changeColor, 1000);
-
-// Abgeschickt
-
-function handleButtonClick() {
-  // console.log("button wurde geklickt");
-  socket.emit("serverEvent", { type: "clickStart" });
-  $(".button").css("pointer-events", "none");
+function handleStartClick() {
+  counter = 0;
+  gameState = "RUNNING";
+  // Shuffle and send the color array to the client
+  colorRow = shuffle(colorRow);
+  socket.emit("serverEvent", { type: "colorSet", color: colorRow });
+  $(".button1").hide();
+  $(".button2").hide();
 }
 
-socket.on("connected", function (msg) {
-  console.log(msg);
-});
+function handleReadyClick() {
+  // Player is ready event
+  socket.emit("serverEvent", { type: "clickReady" });
 
-// Incoming events
+  //FIXME: Comment out now for testing purpose
+  // Hides the button after press
+  // $(".button1").hide();
 
-socket.on("serverEvent", function (message) {
-  console.log(message);
+  // Shows the start button if everyone is ready
+  if (clickCounter === 3) {
+    $(".button2").show();
+  }
+}
 
-  let button1 = document.getElementById("button1");
-
-  // Jeder bereit?
-  if (message.type == "clickStart") {
+/**
+ * Listen to all server events
+ */
+socket.on("serverEvent", (message) => {
+  // Count if everyone is ready
+  if (message.type == "clickReady") {
     clickCounter++;
-    console.log(userList.length);
-    console.log(clickCounter);
+  }
 
-    if (clickCounter == userList.length) {
-      if (myIndex == 0) {
-        colorRow = shuffle(colorRow);
-        socket.emit("serverEvent", { type: "colorSet", color: colorRow });
-      }
-    }
+  // Changes the color if the game starts
+  if (message.type == "colorSet") {
+    changeColor(message.color);
+  }
 
-    if (message.type == "colorSet") {
-      changeColor(message.color);
-      console.log(message.color);
-    }
+  if (message.type == "wrongColor") {
+    // Game reset after wrong button press
+    console.log("GAME RESET");
+    resetGame();
   }
 });
 
+function resetGame() {
+  gameState = "STOP";
+  clickCounter = 0;
+  counter = 10;
+
+  $("#colorButton").hide();
+
+  $(".buttonviolet").css("opacity", "20%");
+  $(".buttonviolet").show();
+
+  $(".buttonyellow").css("opacity", "20%");
+  $(".buttonyellow").show();
+
+  $(".buttonpink").css("opacity", "20%");
+  $(".buttonpink").show();
+
+  $(".buttonblue").css("opacity", "20%");
+  $(".buttonblue").show();
+
+  getPlayerTiles();
+
+  $(".button1").show();
+}
+
+/**
+ * Create a new user and assigns it to a player and color
+ */
 socket.on("newUsersEvent", function (gmyID, gmyIndex, guserList) {
   console.log("New users event: ");
   console.log("That's me: " + gmyID);
@@ -96,30 +169,36 @@ socket.on("newUsersEvent", function (gmyID, gmyIndex, guserList) {
   myIndex = gmyIndex;
   userList = guserList;
 
-  if (myIndex == 0) {
-    $(".buttonviolet").text("You");
-    $(".buttonviolet").css("opacity", "100%");
-    $(".buttonviolet").css("background-color", "#534E8C");
-  }
-
-  if (myIndex == 1) {
-    $(".buttonyellow").text("You");
-    $(".buttonyellow").css("opacity", "100%");
-    $(".buttonyellow").css("background-color", "#E4AD27");
-  }
-
-  if (myIndex == 2) {
-    $(".buttonpink").text("You");
-    $(".buttonpink").css("opacity", "100%");
-    $(".buttonpink").css("background-color", "#FB8D8F");
-  }
-
-  if (myIndex == 3) {
-    $(".buttonblue").text("You");
-    $(".buttonblue").css("opacity", "100%");
-    $(".buttonblue").css("background-color", "#398B9D");
-  }
+  getPlayerTiles();
 });
+
+function getPlayerTiles() {
+  switch (myIndex) {
+    case 0:
+      $(".buttonviolet").text("You");
+      $(".buttonviolet").css("opacity", "100%");
+      $(".buttonviolet").css("background-color", "#534E8C");
+      break;
+    case 1:
+      $(".buttonyellow").text("You");
+      $(".buttonyellow").css("opacity", "100%");
+      $(".buttonyellow").css("background-color", "#E4AD27");
+      break;
+    case 2:
+      $(".buttonpink").text("You");
+      $(".buttonpink").css("opacity", "100%");
+      $(".buttonpink").css("background-color", "#FB8D8F");
+      break;
+    case 3:
+      $(".buttonblue").text("You");
+      $(".buttonblue").css("opacity", "100%");
+      $(".buttonblue").css("background-color", "#398B9D");
+      break;
+    default:
+      console.error("Invalid player index");
+      break;
+  }
+}
 
 /**
  * Shuffles array in place.
